b9fd76119fa3dad7033e3fa16909f2fcc2941ad8
AlijonXr <alixurshetov@gmail.com>
2021-12-09 18:27:10 +0500

    Fulan Update

--- lib/variables/variables.dart
+++ lib/variables/variables.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -9,19 +7,8 @@ class Variables {
   static String? userToken;
   static String? role;
   static SharedPreferences? prefs;
-
   static ValueNotifier<int> currentScreenIndexNotifier = ValueNotifier<int>(0);
-
-  static double speed = 1.0;
-
-  static File? newUserPhoto;
-  static String? newUserName;
-  static String? newUserPhone;
-
   static ValueNotifier<DownloadState> downloadNotifier =
       ValueNotifier(DownloadState.notDonwloaded);
-
   static ValueNotifier<bool> downloadedBookNotifier = ValueNotifier(true);
-
-  // reference to our single class that manages the database
 }
